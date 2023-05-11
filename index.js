e09c0af22d7484d77eaf75d38c5bb772c0c8e9f4
pjananth22123 <pjananth22123@gmail.com>
2023-05-11 17:57:45 +0530

    Added services to use firestore

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config({ path: __dirname + '/.env' });
 const port = process.env.PORT || 3500;
+process.env.TZ = "Asia/Calcutta";
 
 const app = express();
+
+const admin=require('firebase-admin');
+
+console.log(process.env.SERVICE_ACCOUNT);
 
 const whitelist = ['https://yoursite.com','http://localhost:3500'];
 const corsOptions = {
@@ -15,6 +21,56 @@ app.use(cors(corsOptions));
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+
+var serviceAccount = require('./service.json');
+/** var serviceAccount = process.env.SERVICE_ACCOUNT;**/
+admin.initializeApp({credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://project-id.firebaseio.com",
+    authDomain: "project-id.firebaseapp.com",}); 
+
+var db=admin.firestore();
+/** var formRef= db.collection("multiPageFormCol");
+
+formRef.get()
+       .then((value) => value.forEach((doc) => {
+        console.log(doc.data().creationTime.toDate().toLocaleString());
+        console.log(doc.id);
+        }
+        ))
+       .catch((error) => console.log("Error"+error)); **/
+ app.get('/read/:id', async(req,res) => {
+    try{
+        const collRef = db.collection("multiPageFormCol");
+        const response = await collRef.where('email','==',req.params.id).get();
+        let responseArr = [];
+        console.log("Inside Get Call",response.size);
+        if(response.size >= 1){
+        response.forEach((doc) => {
+            let currData = {};
+            currData.email = doc.data().email;
+            currData.firstName = doc.data().firstName;
+            currData.id = doc.id;
+            currData.lastName = doc.data().lastName;
+            currData.other = doc.data().other;
+            currData.creationTime = doc.data().creationTime.toDate().toLocaleString();
+            currData.nationality = doc.data().nationality;
+            currData.middleName = doc.data().middleName;
+            console.log("Response:",currData);
+            responseArr.push(currData);
+        });
+        res.type('application/json');
+        res.send(JSON.stringify(responseArr));
+    }
+    else {
+        res.statusCode = 404;
+        res.type('application/json');
+        res.send(JSON.stringify(responseArr));
+    }       
+    } catch(error) {
+        console.log("Error",error);
+        res.send(error);
+    }
+}); 
 
 app.use('/banking/emiCalc',require('./router/emiCalc'));
 app.use('/banking/loanOnboard',require('./router/loanOnboard'));
